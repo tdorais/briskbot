f48e371fc0f7098a73f5d3ad3d3ae6b5131c88f6
Tim <tdorais@gmail.com>
2018-12-19 23:20:37 -0800

    project re-org

---
+++ src/HttpAccess.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace briskbot
+{
+    public class HttpAccess
+    {
+
+    }
+}
---
+++ tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace briskbot.tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void EasyA()
+        {
+            Assert.True(true);
+        }
+    }
+
+}
